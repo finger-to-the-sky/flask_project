f45cec867000c89ad868ef0ebc45dfa9f9a898a8
maksik <mzagrebam@gmail.com>
2023-04-13 08:50:28 +0300

    add permissions events plugins

--- blog/api/article.py
+++ blog/api/article.py
@@ -1,3 +1,4 @@
+from combojsonapi.event.resource import EventsResource
 from flask_combo_jsonapi import ResourceList, ResourceDetail
 
 from blog.models import Articles
@@ -5,7 +6,19 @@ from blog.schemas import ArticleSchema
 from blog.extensions import db
 
 
+class ArticleListEvent(EventsResource):
+
+    def event_get_count(self):
+        return {'count': Articles.query.count()}
+
+
+class ArticleDetailEvent(EventsResource):
+    def event_get_count_by_author(self, **kwargs):
+        return {'count': Articles.query.filter(Articles.author_id == kwargs['id']).count()}
+
+
 class ArticleList(ResourceList):
+    events = ArticleListEvent
     schema = ArticleSchema
     data_layer = {
         'session': db.session,
@@ -14,6 +27,7 @@ class ArticleList(ResourceList):
 
 
 class ArticleDetail(ResourceDetail):
+    events = ArticleDetailEvent
     schema = ArticleSchema
     data_layer = {
         'session': db.session,
