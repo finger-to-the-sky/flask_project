86cd5c2ac06195417ddf09f9af8057a6f4c360b5
maksik <mzagrebam@gmail.com>
2023-04-17 10:26:59 +0300

    add edit and delete buttons with confirm delete window and endpoints

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,12 @@
+from flask import render_template
+
 from blog.app import create_app
+from blog.models import Articles
 
 app = create_app()
+
+
+@app.route("/")
+def main_page():
+    articles = Articles.query.all()
+    return render_template('articles/list.html', articles=articles)
